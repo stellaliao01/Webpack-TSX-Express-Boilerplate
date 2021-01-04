15e17cf4998c91777628df386caa0b163b4a0586
Anthony Magallanes <anthonymagallanes24@gmail.com>
2021-01-04 14:27:57 -0800

    Typescript Webpack Build

---
+++ README.md
@@ -0,0 +1,9 @@
+Typescript Webpack Config
+
+npm run build
+npm run dev
+
+server localhost 3000
+client localhost 8080
+
+Have fun with Typescript!
---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles/styles.scss'
+
+class App extends Component {
+    render() {
+        return (
+            <div>Have fun with Typescript</div>
+        )
+    }
+}
+
+export default App;
